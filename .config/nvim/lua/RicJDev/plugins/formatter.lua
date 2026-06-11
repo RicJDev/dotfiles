6e466fb7e663790e554d945f1e4d656617de5f29
Ricardo J. M. Mosqueda <ric.mmosqueda@gmail.com>
2026-06-11 10:54:35 -0400

    refactor: eliminando opciones innecesarias

--- .config/nvim/lua/RicJDev/plugins/formatter.lua
+++ .config/nvim/lua/RicJDev/plugins/formatter.lua
@@ -1,26 +1,5 @@
 return {
   "stevearc/conform.nvim",
-  opts = {
-    prettier = {
-      config_command = "--config",
-      config_names = {
-        ".prettierrc",
-        ".prettierrc.json",
-        ".prettierrc.yml",
-        ".prettierrc.yaml",
-        ".prettierrc.json5",
-        ".prettierrc.js",
-        ".editorconfig",
-      },
-      config_path = ".prettierrc.json",
-      ft_parsers = {
-        astro = "astro",
-      },
-      ext_parsers = {
-        astro = "astro",
-      },
-    },
-  },
   config = function()
     require("conform").setup {
       formatters_by_ft = {
