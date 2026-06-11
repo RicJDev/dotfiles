80b5e60af753591f1220e6700e3a66690daf2ce1
Ricardo J. M. Mosqueda <ric.mmosqueda@gmail.com>
2026-06-11 10:53:03 -0400

    feat: agregando mason-lsp para no manejar la config directamente

--- .config/nvim/lua/RicJDev/plugins/lsp-stuff.lua
+++ .config/nvim/lua/RicJDev/plugins/lsp-stuff.lua
@@ -8,4 +8,15 @@ return {
     "mason-org/mason.nvim",
     opts = {},
   },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls", "gopls", "clangd", "rust_analyzer",
+        "ts_ls", "basedpyright", "jsonls", "html", "cssls",
+        "emmet_ls", "astro",
+      },
+      automatic_installation = true,
+    },
+  },
 }
